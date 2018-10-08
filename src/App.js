d944b2b2eec45876f439073fdd3ea5302cd3c23b
Wanda <doubleuevans@gmail.com>
2018-10-08 11:21:49 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      Avengers: [
+        {name: 'IronMan'},
+        {name: 'The Hulk'},
+        {name: 'Captain America'},
+        {name: 'Thor'},
+        {name: 'Hawkeye'},
+        {name: 'Black Widow'},
+      ]
+    }
+  }
+
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2>
+            The Avengers: by Wanda
+          </h2>
+          <h3>{this.state.Avengers.map((char) => {
+            return (
+              <div>{char.name}</div>
+            )
+          })}</h3>
         </header>
       </div>
     );
